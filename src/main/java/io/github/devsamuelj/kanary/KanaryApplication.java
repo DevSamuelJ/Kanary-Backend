b26c4ca9e26bef9aa4904eddd8859fb222d3b903
devsamuelj <samuel701silva@gmail.com>
2025-10-16 20:06:55 -0300

    feat(server): importante implementação dando inicio a estrutura e lógica do server-side (#18)

---
+++ src/main/java/io/github/devsamuelj/kanary/KanaryApplication.java
@@ -0,0 +1,13 @@
+package io.github.devsamuelj.kanary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devsamuelj/kanary/KanaryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devsamuelj.kanary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
