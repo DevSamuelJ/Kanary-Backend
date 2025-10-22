833f2c4c0807694cbf988cbf9a5a0f7a3ae9880a
diegodasp88 <diego.gps@gmail.com>
2025-10-22 20:36:27 -0300

    Voltando as configuracoes anteriores

--- src/test/java/io/github/devsamuelj/kanary/KanaryApplicationTests.java
+++ src/test/java/io/github/devsamuelj/kanary/KanaryApplicationTests.java
@@ -2,13 +2,12 @@ package io.github.devsamuelj.kanary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class KanaryApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-}
 
+	@Test
+	void contextLoads() {
+	}
+
+}
