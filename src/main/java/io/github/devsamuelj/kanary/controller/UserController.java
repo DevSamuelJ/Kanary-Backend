0a5fc20f245deba1b358130c64a70667e79bf96f
diegodasp88 <diego.gps@gmail.com>
2026-01-07 07:18:00 -0300

    melhorias backend: handler, exception, mapper, dtos response, request e error.

--- src/main/java/io/github/devsamuelj/kanary/controller/UserController.java
+++ src/main/java/io/github/devsamuelj/kanary/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
-public class UsersController {
+public class UserController {
 
     @Autowired
     private UserService userService;
