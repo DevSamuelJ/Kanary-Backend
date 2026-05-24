e5712965ad864564f5378750caff127f1f6874b2
devsamuelj <samuel701silva@gmail.com>
2026-05-24 11:12:08 -0300

    fix: regulando commits

--- src/main/java/io/github/devsamuelj/kanary/exception/PasswordNotExistsException.java
+++ src/main/java/io/github/devsamuelj/kanary/exception/PasswordNotExistsException.java
@@ -2,6 +2,7 @@ package io.github.devsamuelj.kanary.exception;
 
 public class PasswordNotExistsException extends RuntimeException {
     public PasswordNotExistsException(String message) {
+
         super(message);
     }
 }
